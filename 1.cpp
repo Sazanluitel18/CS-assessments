975f976079b2b837004ccf1d5c9699d29e3dd447
Aryan Luitel Bolakhe <aryanluitel2024@gmail.com>
2024-11-09 17:07:48 -0600

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -2,7 +2,7 @@
 
 int main() {
     
-    std::cout <<"Alabama A & M University" << std::endl;
+
     std::cout << "Aryan Luitel Bolakhe" << std::endl;
     std::cout << "September 6 2024" << std::endl;
     return 0 ;
